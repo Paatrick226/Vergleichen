43f0fc4b35dbb0e1a226d77b3e5a962c9a339590
Paddy <paddy.zemann@gmail.com>
2022-08-18 20:38:38 +0200

    Projektdateien hinzufügen.

---
+++ Vergleichen/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Vergleichen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("1");
+            list.Add("3");
+            list.Add("5");
+            list.Add("7");
+            //list.Add("12345");
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            List<string> list2 = new List<string>();
+
+                        
+            DataTable dt = new DataTable();
+            dt.Columns.Add("name");
+            dt.Columns.Add("other");
+            dt.Rows.Add("1","2");
+            dt.Rows.Add("3", "4");
+            dt.Rows.Add("5", "6");
+            dt.Rows.Add("7", "8");
+
+            foreach (DataRow r in dt.Rows)
+            {
+                Console.WriteLine(r["name"].ToString() + " - " + r["other"].ToString());
+            }
+
+
+            List<string> s = dt.AsEnumerable().Select(x => x[0].ToString()).ToList();
+
+            foreach (string e in s)
+                Console.WriteLine(e);
+            
+            Console.WriteLine("Vergleichen: ");
+            List<string> firstNotSecond = list.Except(s).ToList();
+            
+            
+
+            if(firstNotSecond.Count != 0)
+            {
+                Console.WriteLine("ungleich!!");
+                Console.WriteLine("Daten in Lise 1 nicht in Liste 2:");
+                foreach (string x in firstNotSecond)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            else
+            {
+                Console.WriteLine("gleich");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
